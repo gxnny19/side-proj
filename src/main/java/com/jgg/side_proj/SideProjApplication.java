8f127610b6cc9d9bc778714cc284351570a60401
wkdrudgus01 <wkdrudgus01@gmail.com>
2025-11-11 21:58:04 +0900

    11/11

--- src/main/java/com/jgg/side_proj/SideProjApplication.java
+++ src/main/java/com/jgg/side_proj/SideProjApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SideProjApplication {
 
+//    http://localhost:8081/swagger-ui/index.html
 	public static void main(String[] args) {
 		SpringApplication.run(SideProjApplication.class, args);
 	}
